454559bd6629f1bb62355e124721a7823fe0889c
MaxZ0030 <maxzhou0030@utexas.edu>
2025-01-08 10:43:26 -0600

    First Commit

---
+++ components/NavBar.jsx
@@ -0,0 +1,73 @@
+"use client"; 
+import Link from "next/link";
+import { usePathname } from "next/navigation"; 
+
+
+export default function NavBar() {
+  const pathname = usePathname(); 
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link href="/" className="navbar-brand">
+          My Website
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link
+                href="/"
+                
+                className={`nav-link ${pathname === "/" ? "active" : ""}`}
+                aria-current={pathname === "/" ? "page" : undefined}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                href="/about"
+                className={`nav-link ${pathname === "/about" ? "active" : ""}`}
+                aria-current={pathname === "/about" ? "page" : undefined}
+              >
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                href="/projects"
+                className={`nav-link ${pathname === "/projects" ? "active" : ""}`}
+                aria-current={pathname === "/projects" ? "page" : undefined}
+              >
+                Projects
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link
+                href="/contact"
+                className={`nav-link ${pathname === "/contact" ? "active" : ""}`}
+                aria-current={pathname === "/contact" ? "page" : undefined}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
