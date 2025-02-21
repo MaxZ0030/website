3159f7993d123e8264b04b124c6d0eb3589a9f72
MaxZ0030 <maxzhou0030@utexas.edu>
2025-02-20 20:16:24 -0600

    Added themes

--- app/projects/page.js
+++ app/projects/page.js
@@ -8,7 +8,7 @@ export default function ProjectsPage() {
       id: 7,
       title: "That Time I Got Reincarnated as a Gun and Had to Buy People to Protect My House (2025)",
       description:
-        "Created in Godot by Max Zhou, Meggie Cheng, Ariz Taqvi, and Carlos Camacho for Pirate Jam 16 in Godot. A 3D Voxel art Tower Defense where the main character summmons human turrets to fight weapnos.",
+        "Created in Godot by Max Zhou, Meggie Cheng, Ariz Taqvi, and Carlos Camacho for Pirate Jam 16 in Godot. A 3D Voxel art Tower Defense where the main character summmons human turrets to fight weapons. Theme: You are the weapon",
       image: "/assets/DrkpHg.png",
       link: "https://tauntyazagoos.itch.io/that-time-i-got-reincarnated-as-a-gun-and-had-to-buy-people-to-protect-my-house",
       linktext: "Click here to go play/download the game!",
@@ -26,7 +26,7 @@ export default function ProjectsPage() {
       id: 5,
       title: "Look Sharp, Major! (2024)",
       description:
-        "Created in Godot by Max Zhou, Meggie Cheng and Ariz Taqvi for GMTK Game Jam 2024. A bullet hell inspired by Vampire Survivors to the tune of 'In the Hall of the Mountain King' by Edvard Grieg.",
+        "Created in Godot by Max Zhou, Meggie Cheng and Ariz Taqvi for GMTK Game Jam 2024. A bullet hell inspired by Vampire Survivors to the tune of 'In the Hall of the Mountain King' by Edvard Grieg. Theme: Built to scale",
       image: "/assets/LSMajor.png",
       link: "https://tauntyazagoos.itch.io/look-sharp-major",
       linktext: "Click here to go play/download the game!",
@@ -35,7 +35,7 @@ export default function ProjectsPage() {
       id: 4,
       title: "Dew (2024)",
       description:
-        "Created in Godot by Max Zhou, Meggie Cheng and Ariz Taqvi for Pixel Game Jam 2024. A puzzle platformer where you can utilize form transformations to solve puzzles.",
+        "Created in Godot by Max Zhou, Meggie Cheng and Ariz Taqvi for Pixel Game Jam 2024. A puzzle platformer where you can utilize form transformations to solve puzzles. Theme: Aqua",
       image: "/assets/DewBanner.png",
       link: "https://tauntyazagoos.itch.io/dew",
       linktext: "Click here to go play/download the game!",
