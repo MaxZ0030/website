4b46aa4e5ef8f5b358a12f51ca8aa78053837b7e
Max Zhou <maxzhou@andrew.cmu.edu>
2026-02-05 09:51:28 -0500

    blog 2

---
+++ app/blog/TheLearningCurveInGames/page.js
@@ -0,0 +1,81 @@
+"use client";
+import ContentContainer from "../../../components/ContentContainer";
+
+export default function BlogLandingPage() {
+  return (
+    
+    <div className="content-card fade-in blog-card">
+      <main style={{ position: 'relative', zIndex: 1, padding: '1rem' }}>
+        <h2 style={{ textAlign: 'center' }}>The Learning Curve in Games</h2>
+        <br />
+        <br />
+        <br />
+        <br />
+        <p>
+          If you're reading this post, you've probably played a game at some point. Whether it's tag or chess, to AAA games like Assassin's Creed or Minecraft, 
+          you've most certainly played some type of game. And if you reach into those banks of memory, you might even consider that game fun.
+        </p>
+        <p>
+          Let's switch gears for a minute. Think of the most boring class you've taken. There was probably something there that made it difficult or challenging. Maybe the teacher was incredibly boring. But suddenly, learning was not fun anymore.
+        </p>
+        <p>
+          However, in both instances, you were actually engaging in the process of learning. Except in one case, you were happily learning about the world, the mechanics, 
+          the gameplay, while in the other scenario, you're stuck in a dreary classroom, unfocused, and not understanding. How in the world does that work?
+        </p>
+
+        <p>
+          Some elements of this may include: games try really hard to tune the learning curve for you. Game designers fanatically try to balance the amount of interest you have compared to the amount of difficulty you face at one given time. 
+          This is one reason why games have levels: both the player-based kind and the stage kind.
+        </p>
+
+        <h3 style={ { textAlign: 'center' } }>I'm getting Stronger!</h3>
+        <br />
+        <p>
+          An abundance of video games these days have 'levels', allowing the player to grow stronger. Skill points and skill trees, player stats and unlocks, all come together in a well curated experience in order to entice players into the game. 
+          An easy of example of this is in DND. Players start off simply, with limited amounts of actions and resources, in order to get them used to the game and using their imagination. 
+          If they're a complete beginner, they learn what in the world a spell slot is, how actions and bonus actions and reactions work, etc. 
+          Slowly but surely, they unlock more and more spells in their repertoire, actions they can use, and unlock their fullest of imaginations, before wishing someone to their deaths. 
+          There was indeed a learning curve, but careful curation doesn't make it feel like you're actually learning anything. But ask anyone with over 100 hours in DND, and they could easily tell you how a round of combat is suppose to go.
+        </p>
+        
+        <h3 style={ { textAlign: 'center' } }>Where do I go now?</h3>
+
+
+        <p>
+          In the other example of levels, the stage kind, consider the quintessential Mario. 1-1 is an entirely different feel from Bowser's castle. The jumps are trickier, the map is more lethal, and 
+          the enemies are more challenging. But by the time you reach Bowser's castle, you've learned how to jump, how to avoid enemies, and how to navigate the world.
+        </p>
+        
+        <h3 style={ { textAlign: 'center' } }>The World Is out for you to Explore!</h3>
+
+        <p>
+          Ah yes, sandbox games like Minecraft and Factorio exist. But even these games have their own ways of teaching you. For example, you can't defeat the Ender Dragon the very second you load into the game. 
+          You can't build Tier 3 conveyor belts without building normal conveyor belts first. There are restrictions in place, that allow you to learn the game first. A hidden learning curve.
+        </p>          
+
+        <h3 style={ { textAlign: 'center' } }>But... there really are...</h3>
+
+        <p>
+          Consider games like Fortnite or League of Legends or 2XKO. Every gun is unlocked, every champion is available, and the pool of fighting characters all have moves and combos available to them. 
+          And yet, there are still some people who are able to memorize where all the guns spawn, or what all the champions do, or all the different characters' combos. 
+          
+        </p>
+
+
+        <p>
+            One recent study done in 2024 (The Impact of Educational Games on Learning Outcomes) proves this to be true: the overall effect is that games have a positive effect on boosting motivation and engagement, especially when it comes to learning. 
+            It's also true, that the best teachers that I've had, not only have a way of getting their class to become absolutely engrossed in what they're saying, they also tailor each lesson so that they structure well and build on each other. 
+            It just goes to show that teachers are also designers in their own right.
+        </p>
+
+
+{/* This, is also why the term friendslop has been thrown about so much lately. Low barrier of entry, heavily reliant social interaction games absolutely thrive in the communities of Twitch. */}
+
+        
+
+      </main>
+    </div>
+
+    
+  );
+}
