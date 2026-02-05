49ec0e5fbfd788511a6aa2d5a1889cbfda3ff742
Max Zhou <maxzhou@andrew.cmu.edu>
2026-02-05 09:59:12 -0500

    blog 2 edit

--- app/blog/TheLearningCurveInGames/page.js
+++ app/blog/TheLearningCurveInGames/page.js
@@ -43,29 +43,37 @@ export default function BlogLandingPage() {
 
         <p>
           In the other example of levels, the stage kind, consider the quintessential Mario. 1-1 is an entirely different feel from Bowser's castle. The jumps are trickier, the map is more lethal, and 
-          the enemies are more challenging. But by the time you reach Bowser's castle, you've learned how to jump, how to avoid enemies, and how to navigate the world.
+          the enemies are more challenging. You learned how to jump, how to wall climb, and how to use powerups to their fullest. But even within the levels themselves, there is a learning curve.
+          The first few goombas are easy to stomp on, but as you progress, you learn that there are different types of enemies, ones that walk at you, ones that fly, etc.
+          The learning curve is there, but it's hidden beneath the fun of the game. This is the advantage of games with these carefully curated levels: they control how you're learning. 
+          After all, few games throw everything you can do at you and expect you to figure it out yourself... right?
         </p>
         
         <h3 style={ { textAlign: 'center' } }>The World Is out for you to Explore!</h3>
 
         <p>
           Ah yes, sandbox games like Minecraft and Factorio exist. But even these games have their own ways of teaching you. For example, you can't defeat the Ender Dragon the very second you load into the game. 
-          You can't build Tier 3 conveyor belts without building normal conveyor belts first. There are restrictions in place, that allow you to learn the game first. A hidden learning curve.
+          You can't build Tier 3 conveyor belts without building normal conveyor belts first. There are restrictions in place, that allow you to learn the game first. A hidden learning curve. But most importantly,
+          you're having fun while you're learning, even when your whole brain is focused on figuring out how to do the next thing. 
         </p>          
 
         <h3 style={ { textAlign: 'center' } }>But... there really are...</h3>
 
         <p>
           Consider games like Fortnite or League of Legends or 2XKO. Every gun is unlocked, every champion is available, and the pool of fighting characters all have moves and combos available to them. 
-          And yet, there are still some people who are able to memorize where all the guns spawn, or what all the champions do, or all the different characters' combos. 
+          And yet, there are still some people who are able to memorize where all the guns spawn, or what all the champions do, or all the different characters' combos. Some would say the learning curve for these games is steep.
+          But, these games are still considered fun by millions of people worldwide. Why is that? Surprisingly, it's because the learning curve is still there, but it's not in the game mechanics. It's in the players. Matchmaking
+          is what I consider the key factor to be in these games. By matching players of similar skill levels together, you are allowed to learn at basically your own pace, while also being challenging enough to want to improve.
+          Players of a lower skill level aren't thrown into games with the top of the ladder, because each level of player has their own things they want to learn.
           
         </p>
 
+        <h3 style={ { textAlign: 'center' } }>Bringing it all Together</h3>
 
         <p>
             One recent study done in 2024 (The Impact of Educational Games on Learning Outcomes) proves this to be true: the overall effect is that games have a positive effect on boosting motivation and engagement, especially when it comes to learning. 
             It's also true, that the best teachers that I've had, not only have a way of getting their class to become absolutely engrossed in what they're saying, they also tailor each lesson so that they structure well and build on each other. 
-            It just goes to show that teachers are also designers in their own right.
+            It just goes to show that teachers are also designers in their own right. and that games, when designed well, can be some of the best learning tools out there.
         </p>
 
 
