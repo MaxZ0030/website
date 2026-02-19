c10e4238236c265cd082402b7d756bd57116cc16
Max Zhou <maxzhou@andrew.cmu.edu>
2026-02-19 09:49:16 -0500

    Runback

---
+++ app/blog/TheRunbackJustOneMoreTime/page.js
@@ -0,0 +1,77 @@
+"use client";
+import ContentContainer from "../../../components/ContentContainer";
+
+export default function BlogLandingPage() {
+  return (
+    
+    <div className="content-card fade-in blog-card">
+      <main style={{ position: 'relative', zIndex: 1, padding: '1rem' }}>
+        <h2 style={{ textAlign: 'center' }}>The Learning Curve in Games</h2>
+        <br />
+        <br />
+        <br />
+        <br />
+        <p>
+          You're at the gates to the boss. You've prepped for this. Or so you think. You take a deep breath. It's time.
+        </p>
+        <p>
+          Five seconds later, you're crawling on the ground on one HP, before getting clobbered by the boss's most basic of attacks. 
+          When you respawn, however, it's not in front of those pearly gates to the boss door. You've got a long road ahead of you to get back to where you were. 
+          Maybe it's a whole gauntlet of enemies that you snuck through on your first time. (Maybe you're not supposed to be here then?) Maybe it's a difficult platforming challenge. 
+          Maybe these metaphorical gates were actually the enemy's base in a MOBA. Or maybe they were the last few players left in a battle royale. 
+          Whatever the case, only one thought sings through your head: Just one more time.
+        </p>
+        <p>
+          Now, why do we subject ourselves to this seemingly agonizing 'torture'? On a surface level, telling a random person that you spent long hard hours to gain back that progress you lost seems weird, or downright bizarre. 
+          "Move on", they might say. "Don't waste your time". In fact, this is true with alot of games nowadays. Some games don't want you to potentially quit when you die, so they make that death seem easier. 
+          Games have mellowed out over the years. Take Mario, as an example.
+        </p>
+
+
+        <h3 style={ { textAlign: 'center' } }>The Original Runback</h3>
+        {/* <img src="/assets/blog/bg3pic.png" style={{ width: '100%', height: 'auto' }}></img> */}
+        <br />
+
+        <p>
+         Now wait a minute, you might say. In modern Mario, when you die in a level (solo, that is), you're ejected from the level. But, modern Mario has the little helper known as checkpoints.
+        </p>
+        <p>
+          These checkpoints enable us to quickly resume where we left off. But what was it like back then? Losing all of your little Mario lives sent you straight back to world 1-1. 
+          And yet, despite this grueling challenge, people still found it in them to beat the game. And today, people would even call the original Mario a timeless classic. Funny how that works.
+        </p>
+        
+        <h3 style={ { textAlign: 'center' } }>Progress and Challenge</h3>
+        {/* <img src="/assets/blog/factoriopic.jpg" style={{ width: '100%', height: 'auto' }}></img> */}
+        <br />
+
+        <p>
+          One of the things that's important is that games give us a measured sense of progress. I can see exactly how high the boss's health bar is when I fought him last time, and this time, I was able to whittle him down even more. 
+          I know that I was just one combo away from beating that rhythm game. That sense of measured progress, which humans love, is intrinsic to videogames. And it's just partially what makes them so fun to play.
+        </p>
+        
+        
+        <p>
+          Another key component of this so called runback is the stories that come from the challenge. 
+          Hollow Knight players all moan and groan about the Path of Pain. Elden Ring players cry at the sight of Malenia. 
+          Pokémon players shudder whenever they think of Cynthia's team. But what's important is that eventually, these players find themselves swapping stories with each other. "I was this close!" "You'd never believe what happened here!" 
+          There's this relatable feeling that all players who've attempted this challenge understand and try to overcome.
+        </p>          
+        <br />
+        <p>
+          The runback doesn't mean you have to succeed the next time. Just even making the attempt is important. After all, no one forces gamers to sit there and forcibly play the game. And admittedly, some don't. They look upon all the progress they've lost and see despair. 
+          But, to those who are able to get themselves back in the chair and attempt that challenge again and triumph victoriously over the boss that they were attempting to overcome, they were glad that they said: "Just one more time".
+          
+        </p>
+
+
+
+{/* This, is also why the term friendslop has been thrown about so much lately. Low barrier of entry, heavily reliant social interaction games absolutely thrive in the communities of Twitch. */}
+
+        
+
+      </main>
+    </div>
+
+    
+  );
+}
