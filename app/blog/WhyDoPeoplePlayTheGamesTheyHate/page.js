fd2eaa8fc54ce6e463609b957f56f7888af51beb
Max Zhou <maxzhou@andrew.cmu.edu>
2026-03-26 09:51:13 -0400

    newblog

---
+++ app/blog/WhyDoPeoplePlayTheGamesTheyHate/page.js
@@ -0,0 +1,95 @@
+"use client";
+import ContentContainer from "../../../components/ContentContainer";
+
+export default function BlogLandingPage() {
+  return (
+    <div className="content-card fade-in blog-card">
+      <main style={{ position: "relative", zIndex: 1, padding: "1rem" }}>
+        <h2 style={{ textAlign: "center" }}>
+          Why do people play the games they &apos;hate&apos;?
+        </h2>
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <p>
+          Oh, would you look at that. It happened again. Yet another Twitter post
+          showcasing a video of some person throwing his monitor across the room
+          in a fit of rage as he screams petulantly. Most people shake their head
+          from left to right, solemnly. Some people laugh at the antics of what
+          could very well be just another fake video. But, there is definitely a
+          group of people that look at that video and find it relatable.
+        </p>
+        <p>
+          Maybe their own Marvel Rivals game was a complete stomp. Maybe their
+          Valorant game didn&apos;t go so well as planned. Maybe they didn&apos;t get
+          the deck they wanted in Slay the Spire 2. Maybe Radahn keeps stomping
+          their face in Elden Ring. In the moment, all these players come to the
+          same conclusion: &apos;I hate this game&apos;. Well then, why do they keep
+          coming back for more?
+        </p>
+
+        <h3 style={{ textAlign: "center" }}>Two Sides of the Spectrum</h3>
+        <br />
+
+        <p>
+          One key draw of all of these games is the idea of this sort of nebulous
+          luck that we cannot control. And that makes it easy to blame the game
+          itself. Of course matchmaking had to put you with terrible players, or
+          the enemy team was filled with people who are really good at the game.
+        </p>
+        <p>
+          But for every &apos;unlucky&apos; experience players have, they are met with
+          an equally lucky, adrenaline-filling, dopamine-coursing moment. That
+          one godlike run in Hades 2 where you get all the augments you like.
+          That time in Crab Champions where you were able to go to 100 rounds
+          without fail. However, no one really tells us about the times where
+          stuff went right.
+        </p>
+        <p>
+          More often than not, what we hear are complaints about the times things
+          went wrong, even though the amount of times things went well and things
+          went terribly should be about equal. To that end, we can turn to the
+          maxim: &apos;If you are complaining, you tell everyone. If something goes
+          well, you tell one person.&apos;
+        </p>
+
+        <h3 style={{ textAlign: "center" }}>Forever Sinking</h3>
+        <br />
+
+        <p>
+          Another compelling factor that keeps players playing games they
+          &apos;hate&apos; is sunk cost fallacy. People are reluctant to abandon these
+          cursed games because they&apos;ve dedicated so much time, effort, and
+          sometimes money into it. Consider the hours spent grinding for
+          Primogems in Genshin Impact, or the amount of loot and skins you&apos;ve
+          amassed across the years in Overwatch.
+        </p>
+        <p>
+          Alot of the time, it&apos;s not so easy to put down. These accounts have
+          been with us for years and years. To some, they value these accounts
+          more than their own children, all the while maintaining their love hate
+          relationship with the game. Maybe their accounts and children have more
+          in common than they think.
+        </p>
+
+        <h3 style={{ textAlign: "center" }}>A Glimmer of Hope</h3>
+        <br />
+
+        <p>
+          At the end of the day, there probably is something there that keeps a
+          player playing. Long-time League of Legends players will tell others
+          that League is fun, but you shouldn&apos;t get into it. Yet, these players
+          are notorious for being some of the most toxic, unfriendly community
+          members to exist. An interesting contradiction.
+        </p>
+        <p>
+          Amidst the screaming and yelling and punching computers, League
+          players, at the end of the day, will still consider their game fun.
+          And isn&apos;t that what&apos;s most important?
+        </p>
+      </main>
+    </div>
+  );
+}
