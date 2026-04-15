571e59075441a4dc349d7391ed2efaaaa2f6b5e5
Max Zhou <maxzhou@andrew.cmu.edu>
2026-04-15 16:05:05 -0400

    ModernMMO

---
+++ app/blog/TheMysteryofTheModernMMO/page.js
@@ -0,0 +1,102 @@
+"use client";
+import ContentContainer from "../../../components/ContentContainer";
+
+export default function BlogLandingPage() {
+  return (
+    <div className="content-card fade-in blog-card">
+      <main style={{ position: "relative", zIndex: 1, padding: "1rem" }}>
+        <h2 style={{ textAlign: "center" }}>Mystery of the Modern MMORPG</h2>
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <p>
+          Picture this. It&apos;s the early 2000&apos;s. You get home from school and
+          boot up your computer. You&apos;ve told your friends that you promised to
+          join them as soon as you got home, after all. After waiting a few
+          minutes for it to load, there it is: World of Warcraft. You log in,
+          join the raid among your friends, and off you are to join ferocious
+          raids, hunt terrifying creatures, but most importantly, create the
+          stories that will last for a lifetime.
+        </p>
+        <p>
+          The fantasy of an MMORPG is one that is constantly promised to us.
+          Even if you haven&apos;t explicitly experienced the moment I just
+          described, you could definitely imagine it. Simpler times, nostalgic
+          times, the promise of some fantasy that&apos;s hard to come by today.
+          But... why is that? With the concept of videogames and especially
+          online videogames in the full modern sphere, why is it that the
+          MMORPG, one of the quintessential cornerstones of the online videogame
+          experience, seems to be a dying genre?
+        </p>
+
+        <h3 style={{ textAlign: "center" }}>The Grind... and the Reality</h3>
+        <br />
+
+        <p>
+          MMORPGs often sell us a fantasy. Cool, glowing gear, all
+          powerful-looking weapons, glorious mounts and homes decked out with
+          beautiful interiors. However, what is the cost of getting there? The
+          answer is the one resource that we don&apos;t have enough of: time. The
+          amount of time it takes for someone to grind out that piece of
+          legendary gear is... paradoxical, to say the least.
+        </p>
+        <p>
+          If everyone knows that the piece of gear you wear can only be obtained
+          through hundreds of hours of grind, then when you wear it, you feel
+          like a king. But putting in that much effort into grinding out a piece
+          of gear can be... time-consuming, for lack of a better term. And few
+          people want to do that. Maybe we should just let them get that piece
+          of gear more easily.
+        </p>
+        <p>
+          But therein lies the paradox. If a piece of gear is so easy to obtain
+          that everyone has it, it loses a level of prestige. If something so
+          &apos;legendary&apos; could be bought at any time, then it wouldn&apos;t be
+          worth anything. So, here comes another difficult question when it
+          comes to MMORPGs. How do you get players to invest tons of time into a
+          game, but also make it so that they willingly do so to hit that goal
+          they once sought? Not an easy question for sure.
+        </p>
+
+        <h3 style={{ textAlign: "center" }}>The Fantasy of Time</h3>
+        <br />
+
+        <p>
+          And perhaps that&apos;s the biggest fantasy. There&apos;s simply such an
+          imbalance to the amount of time players can spend playing at once. Of
+          course, when we&apos;re young and able to play lots, then kids are able
+          to put in all the time and resources they want into a game. But
+          evidence has shown that the average attention span and the amount of
+          time children spend focused on a task is dwindling.
+        </p>
+
+        <h3 style={{ textAlign: "center" }}>What Can Be Done?</h3>
+        <br />
+
+        <p>
+          The problem with MMORPGs is that they take an incredible amount of
+          time and effort to develop. From a realistic standpoint, it&apos;s hard
+          to commit so many man hours and time from people, without knowing the
+          potential success of the game. Making a true-blue MMORPG in this day
+          and age is way too risky.
+        </p>
+        <p>
+          This is why some games have pivoted. On one hand, games like Genshin
+          Impact and Wuthering Waves try to deliver on a simplified idea of the
+          MMORPG genre. There&apos;s a lot in these games that could let them
+          classify as an MMORPG. Do they have multiplayer content? To a degree.
+          Is there a way that people can grind for fantastic gear? There is!
+          But one of the biggest differences is that not everyone is on at the
+          same time. So... Genshin is an MMO? But also... an RPG?
+        </p>
+        <p>
+          All in all, the landscape of the modern MMO is something ever
+          changing. Are the multiplayer aspects really being reduced to just
+          singular instance based moments?
+        </p>
+      </main>
+    </div>
+  );
+}
